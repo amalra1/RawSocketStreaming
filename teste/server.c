948cf7ab14e7f859294818fd344c4411b82dca6d
Pedro <amaral1@ufpr.br>
2024-06-05 23:26:06 -0300

    Server conseguiu mandar 1 mensagem pra loopback

--- teste/server.c
+++ teste/server.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
-#include <net/ethernet.h> 
-#include <linux/if_packet.h> 
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netpacket/packet.h>
+#include <net/ethernet.h>
 #include <net/if.h>
-#include "libServer.h" 
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include "libServer.h"
+
+#define BUFFER_LENGTH 4096
 
 int main ()
 {
-    printf("a");
+    char buffer[BUFFER_LENGTH];
 
-    return 0;
+    // Create the raw socket using loopback interface
+    int soquete = cria_raw_socket("lo");
+
+    // Prepare the message to send
+    const char *message = "Hello, loopback!";
+    strcpy(buffer, message);
+
+    // Prepare the destination address
+    struct sockaddr_ll dest_addr = {0};
+    dest_addr.sll_family = AF_PACKET;
+    dest_addr.sll_protocol = htons(ETH_P_ALL);
+    dest_addr.sll_ifindex = if_nametoindex("lo");
+    dest_addr.sll_halen = ETH_ALEN;
+    memset(dest_addr.sll_addr, 0xff, ETH_ALEN);  // Broadcast MAC address for testing
+
+    // Send the message
+    if (sendto(soquete, buffer, strlen(buffer), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) == -1) {
+        perror("Error sending data");
+        close(soquete);
+        return EXIT_FAILURE;
+    }
+
+    printf("Message sent: %s\n", message);
+
+    // Close the raw socket
+    close(soquete);
+
+    return EXIT_SUCCESS;
 }
